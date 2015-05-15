5452e943f97be6a380d54101585341a753e0c5e4
kaziahmed <kazi255@gmail.com>
2015-05-15 16:18:47 -0400

    Programming with Data Structures Java
    Data Structures coded in Java

---
+++ Programming-with-Data-Structures-Java/src/programs/Program1.java
@@ -0,0 +1,152 @@
+package programs;
+
+import java.util.*;
+
+public class Program1 {
+
+	public static void main(String args[]){
+		//LinkedList
+	    LinkedList l2 = new LinkedList();
+	    //add elements to the linked list
+	    l2.add("A");
+	    l2.add("B");
+	    l2.add("C");
+	    l2.add("D");
+	    l2.add("E");
+	    l2.addLast("F");
+	    l2.addFirst("G");
+	    l2.add(1, "H");
+	    System.out.println("Original contents of l2: " + l2);
+
+	    //remove elements from the linked list
+	    l2.remove("F");
+	    l2.remove(2);
+	    System.out.println("Contents of l2 after deletion: " + l2);
+	      
+	    //remove first and last elements
+	    l2.removeFirst();
+	    l2.removeLast();
+	    System.out.println("l2 after deleting first and last: " + l2);
+
+	    //get and set a value
+	    Object val = l2.get(2);
+	    l2.set(2, (String) val + " Changed");
+	    System.out.println("l2 after change: " + l2 + "\n");
+	    
+	    //Date
+	    //Instantiate a Date object
+	    Date date = new Date();
+	        
+	    //Display time and date using toString()
+	    System.out.println(date.toString() + "\n");
+	      
+	    //Array
+	    //Declares an array of integers
+	    int[] anArray;
+
+	    //Allocates memory for 10 integers
+	    anArray = new int[10];
+	           
+	    //Initialize first element
+	    anArray[0] = 100;
+	    //Initialize second element
+	    anArray[1] = 200;
+	    //Continued
+	    anArray[2] = 300;
+	    anArray[3] = 400;
+	    anArray[4] = 500;
+	    anArray[5] = 600;
+	    anArray[6] = 700;
+	    anArray[7] = 800;
+	    anArray[8] = 900;
+	    anArray[9] = 1000;
+
+	    System.out.println("Element at index 0: " + anArray[0]);
+	    System.out.println("Element at index 1: " + anArray[1]);
+	    System.out.println("Element at index 2: " + anArray[2]);
+        System.out.println("Element at index 3: " + anArray[3]);
+	    System.out.println("Element at index 4: " + anArray[4]);
+	    System.out.println("Element at index 5: " + anArray[5]);
+	    System.out.println("Element at index 6: " + anArray[6]);
+	    System.out.println("Element at index 7: " + anArray[7]);
+	    System.out.println("Element at index 8: " + anArray[8]);
+	    System.out.println("Element at index 9: " + anArray[9] + "\n");
+	    
+	    //Arithmetic Operators
+	    int a = 15;
+	    int b = 25;
+	    int c = 30;
+	    System.out.println("a + b = " + (a + b));
+	    System.out.println("a - b = " + (a - b));
+	    System.out.println("a * b = " + (a * b));
+	    System.out.println("b / a = " + (b / a));
+	    System.out.println("b % a = " + (b % a));
+	    System.out.println("c % a = " + (c % a) + "\n");
+	    
+	    //Comparator
+	    class Car implements Comparator<Car>, Comparable<Car>{
+	    private String name;
+	    private int cost;
+	    Car(){
+	    }
+
+	    Car(String n, int a){
+	    	name = n;
+	    	cost = a;
+	    	}
+
+	    public String getCarName(){
+	    	return name;
+	    }
+
+	    public int getCarCost(){
+	    	return cost;
+	    }
+
+	    //Overriding the compareTo method
+	    public int compareTo(Car c){
+	    	return (this.name).compareTo(c.name);
+	    }
+
+	    //Overriding the compare method to sort the cost 
+	    public int compare(Car c, Car c1){
+	    	return c.cost - c1.cost;
+	    	}
+	    }
+	    
+	    //Takes a list of Car objects
+	    List<Car> list = new ArrayList<Car>();
+
+	    list.add(new Car("Tesla Model S",65000));
+	    list.add(new Car("Lexus CT 200h",32200));
+	    list.add(new Car("Audi A3",29900));
+	    list.add(new Car("Acura ILX",27900));
+	    list.add(new Car("Volkswagen Jetta",17325));
+	    //Sorts the array list
+	    Collections.sort(list);
+
+	    for(Car a1: list)
+	    	//Printing the sorted list of names
+	    	System.out.print(a1.getCarName() + ", ");
+
+	    //Sorts the array list using comparator
+	    Collections.sort(list, new Car());
+	    System.out.println(" ");
+	    for(Car a1: list)
+	    	//Printing the sorted list of costs
+	    	System.out.print(a1.getCarName() +": $"+ a1.getCarCost() + ", ");
+	    System.out.println("Car names and costs" + "\n");
+	    
+	    //Printing the recursion
+	    System.out.println(Factorial(6));
+	    } 
+	//Recursive method
+	private static int Factorial(int i) {
+	// TODO Auto-generated method stub
+	if(i == 1)
+	    return 1;
+	else{
+	    return(i*(Factorial(i-1)));
+	}
+	}
+}
